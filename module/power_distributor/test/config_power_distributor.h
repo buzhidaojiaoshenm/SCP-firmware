de0c1f3a4e356d09d0025e0f2854017077509231
Matthew Wellings <matthew.wellings@arm.com>
2024-12-09 08:21:32 +0000

    mod/power-distributor: Add initial tests for module
    Add first (init & bind) tests for the power-distributor module.
    
    Signed-off-by: Matthew Wellings <matthew.wellings@arm.com>

---
+++ module/power_distributor/test/config_power_distributor.h
@@ -0,0 +1,62 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2024, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <mod_power_distributor.h>
+
+#include <fwk_element.h>
+#include <fwk_id.h>
+#include <fwk_module_idx.h>
+
+enum test_power_distributor_domain_idx {
+    TEST_DOMAIN_SOC,
+    TEST_DOMAIN_CPU,
+    TEST_DOMAIN_CPU_BIG,
+    TEST_DOMAIN_CPU_LITTLE,
+    TEST_DOMAIN_GPU,
+    TEST_DOMAIN_COUNT,
+    TEST_DOMAIN_NONE = UINT32_MAX,
+};
+
+enum test_power_controller_api_idx {
+    TEST_CONTROLLER_API_SOC,
+    TEST_CONTROLLER_API_CPU,
+    TEST_CONTROLLER_API_CPU_BIG,
+    TEST_CONTROLLER_API_CPU_LITTLE,
+    TEST_CONTROLLER_API_GPU,
+    TEST_CONTROLLER_API_COUNT,
+};
+
+struct mod_power_distibutor_domain_config test_power_distibutor_domain_config[TEST_DOMAIN_COUNT] = {
+    [TEST_DOMAIN_SOC] = {
+        .parent_idx = TEST_DOMAIN_NONE,
+        .controller_api_id = FWK_ID_API_INIT(
+            FWK_MODULE_IDX_POWER_DISTRIBUTOR,
+            TEST_CONTROLLER_API_SOC),
+    },
+    [TEST_DOMAIN_CPU] = {
+        .parent_idx = TEST_DOMAIN_SOC,
+        .controller_api_id = FWK_ID_NONE,
+    },
+    [TEST_DOMAIN_CPU_BIG] = {
+        .parent_idx = TEST_DOMAIN_CPU,
+        .controller_api_id = FWK_ID_API_INIT(
+            FWK_MODULE_IDX_POWER_DISTRIBUTOR,
+            TEST_CONTROLLER_API_CPU_BIG),
+    },
+    [TEST_DOMAIN_CPU_LITTLE] = {
+        .parent_idx = TEST_DOMAIN_CPU,
+        .controller_api_id = FWK_ID_API_INIT(
+            FWK_MODULE_IDX_POWER_DISTRIBUTOR,
+            TEST_CONTROLLER_API_CPU_LITTLE),
+    },
+    [TEST_DOMAIN_GPU] = {
+        .parent_idx = TEST_DOMAIN_SOC,
+        .controller_api_id = FWK_ID_API_INIT(
+            FWK_MODULE_IDX_POWER_DISTRIBUTOR,
+            TEST_CONTROLLER_API_GPU),
+    },
+};
---
+++ module/power_distributor/test/mod_power_distributor_unit_test.c
@@ -0,0 +1,206 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2024, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     Power capping module unit test.
+ */
+
+#include "config_power_distributor.h"
+#include "scp_unity.h"
+#include "unity.h"
+
+#include <Mockfwk_mm.h>
+#include <Mockfwk_module.h>
+
+#include <mod_power_distributor.h>
+
+#include UNIT_TEST_SRC
+
+struct mod_power_distributor_domain_ctx test_ctx_table[TEST_DOMAIN_COUNT];
+
+struct interface_power_management_api test_power_management_api = { 0 };
+
+void setUp(void)
+{
+    memset((void *)test_ctx_table, 0U, sizeof(test_ctx_table));
+    power_distributor_ctx.domain = test_ctx_table;
+
+    power_distributor_ctx.domain_count = TEST_DOMAIN_COUNT;
+    for (unsigned int i = 0U; i < TEST_DOMAIN_COUNT; i++) {
+        power_distributor_ctx.domain[i].controller_api =
+            &test_power_management_api;
+        power_distributor_ctx.domain[i].config =
+            &test_power_distibutor_domain_config[i];
+    }
+}
+
+void utest_power_distributor_init_success(void)
+{
+    int status;
+    unsigned int element_count = 23U;
+    struct mod_power_distributor_domain_ctx domain = { 0 };
+    power_distributor_ctx.domain_count = 0;
+
+    fwk_mm_calloc_ExpectAndReturn(
+        element_count, sizeof(power_distributor_ctx.domain[0]), &domain);
+
+    status = power_distributor_init(
+        FWK_ID_MODULE(FWK_MODULE_IDX_POWER_DISTRIBUTOR), element_count, NULL);
+    TEST_ASSERT_EQUAL(element_count, power_distributor_ctx.domain_count);
+    TEST_ASSERT_EQUAL(&domain, power_distributor_ctx.domain);
+    TEST_ASSERT_EQUAL(FWK_SUCCESS, status);
+}
+
+void utest_power_distributor_element_init_success(void)
+{
+    int status;
+    fwk_id_t element_id;
+    static struct mod_power_distibutor_domain_config
+        domains_config[TEST_DOMAIN_COUNT] = { 0 };
+
+    for (unsigned int index = 0U; index < TEST_DOMAIN_COUNT; index++) {
+        element_id = FWK_ID_ELEMENT(FWK_MODULE_IDX_POWER_DISTRIBUTOR, index);
+
+        status = power_distributor_element_init(
+            element_id, 30U, &domains_config[index]);
+
+        TEST_ASSERT_EQUAL_PTR(
+            &domains_config[index], power_distributor_ctx.domain[index].config);
+        TEST_ASSERT_EQUAL_PTR(
+            NULL, power_distributor_ctx.domain[index].controller_api);
+        TEST_ASSERT_EQUAL_PTR(
+            NULL, power_distributor_ctx.domain[index].node.children_idx_table);
+        TEST_ASSERT_EQUAL(
+            0, power_distributor_ctx.domain[index].node.children_count);
+        TEST_ASSERT_EQUAL(
+            0, power_distributor_ctx.domain[index].node.data.power_demand);
+        TEST_ASSERT_EQUAL(
+            0, power_distributor_ctx.domain[index].node.data.power_budget);
+        TEST_ASSERT_EQUAL(
+            NO_POWER_LIMIT,
+            power_distributor_ctx.domain[index].node.data.power_limit);
+        TEST_ASSERT_EQUAL(FWK_SUCCESS, status);
+    }
+}
+
+void utest_mod_distributor_bind_round_0(void)
+{
+    int status;
+    unsigned int round = 0U;
+
+    for (unsigned int index = 0U; index < TEST_DOMAIN_COUNT; index++) {
+        fwk_id_t domain_id =
+            FWK_ID_ELEMENT_INIT(FWK_MODULE_IDX_POWER_DISTRIBUTOR, index);
+        struct mod_power_distributor_domain_ctx *domain_ctx =
+            &(power_distributor_ctx.domain[index]);
+
+        if (!fwk_id_is_equal(
+                domain_ctx->config->controller_api_id, FWK_ID_NONE)) {
+            fwk_module_bind_ExpectAndReturn(
+                fwk_id_build_module_id(domain_ctx->config->controller_api_id),
+                domain_ctx->config->controller_api_id,
+                &domain_ctx->controller_api,
+                FWK_SUCCESS);
+        }
+
+        status = power_distributor_bind(domain_id, round);
+
+        TEST_ASSERT_EQUAL(FWK_SUCCESS, status);
+    }
+}
+
+void utest_mod_distributor_bind_invalid_round(void)
+{
+    int status;
+    unsigned int round = 1U;
+
+    for (unsigned int index = 0U; index < TEST_DOMAIN_COUNT; index++) {
+        fwk_id_t domain_id =
+            FWK_ID_ELEMENT_INIT(FWK_MODULE_IDX_POWER_DISTRIBUTOR, index);
+
+        status = power_distributor_bind(domain_id, round);
+
+        TEST_ASSERT_EQUAL(FWK_SUCCESS, status);
+    }
+}
+
+void utest_mod_distributor_process_bind_request(void)
+{
+    int status;
+    fwk_id_t dummy_id = FWK_ID_NONE;
+    const void *api = NULL;
+
+    status = power_distributor_process_bind_request(
+        dummy_id,
+        dummy_id,
+        FWK_ID_API(
+            FWK_MODULE_IDX_POWER_DISTRIBUTOR,
+            MOD_POWER_DISTRIBUTOR_API_IDX_DISTRIBUTION),
+        &api);
+
+    TEST_ASSERT_EQUAL_PTR(&distribution_api, api);
+    TEST_ASSERT_EQUAL(FWK_SUCCESS, status);
+}
+
+void utest_mod_distributor_process_bind_request_invalid_api(void)
+{
+    int status;
+    fwk_id_t dummy_id = FWK_ID_NONE;
+    enum mod_power_distributor_api_idx invalid_api_id =
+        MOD_POWER_DISTRIBUTOR_API_IDX_COUNT;
+    const void *api = NULL;
+
+    status = power_distributor_process_bind_request(
+        dummy_id,
+        dummy_id,
+        FWK_ID_API(FWK_MODULE_IDX_POWER_DISTRIBUTOR, invalid_api_id),
+        &api);
+
+    TEST_ASSERT_EQUAL(FWK_E_PARAM, status);
+}
+
+void utest_mod_distributor_process_bind_request_invalid_module(void)
+{
+    int status;
+    fwk_id_t dummy_id = FWK_ID_NONE;
+    enum fwk_module_idx invalid_module_idx = FWK_MODULE_IDX_COUNT;
+    const void *api = NULL;
+
+    status = power_distributor_process_bind_request(
+        dummy_id,
+        dummy_id,
+        FWK_ID_API(
+            invalid_module_idx, MOD_POWER_DISTRIBUTOR_API_IDX_DISTRIBUTION),
+        &api);
+
+    TEST_ASSERT_EQUAL(FWK_E_PARAM, status);
+}
+
+void tearDown(void)
+{
+}
+
+int power_distributor_test_main(void)
+{
+    UNITY_BEGIN();
+
+    RUN_TEST(utest_power_distributor_init_success);
+    RUN_TEST(utest_power_distributor_element_init_success);
+    RUN_TEST(utest_mod_distributor_bind_round_0);
+    RUN_TEST(utest_mod_distributor_bind_invalid_round);
+    RUN_TEST(utest_mod_distributor_process_bind_request);
+    RUN_TEST(utest_mod_distributor_process_bind_request_invalid_api);
+    RUN_TEST(utest_mod_distributor_process_bind_request_invalid_module);
+
+    return UNITY_END();
+}
+
+#if !defined(TEST_ON_TARGET)
+int main(void)
+{
+    return power_distributor_test_main();
+}
+#endif
