558541c9431f2103e80ab5d884035523fd8a58a2
fsylvestre <fsylvestre@baylibre.com>
2025-07-18 18:15:01 +0200

    rcar/product: fix rcar CMake build The binary generated by CMake build was not booting, the issue is link to the introduction of libc usage that suppose MMU initialization, which is not the case for CortexA53 platform.
    Signed-off-by: Florian Sylvestre <fsylvestre@baylibre.com>

--- product/rcar/scp_ramfw/Toolchain-GNU.cmake
+++ product/rcar/scp_ramfw/Toolchain-GNU.cmake
@@ -1,6 +1,6 @@
 #
 # Arm SCP/MCP Software
-# Copyright (c) 2021, Arm Limited and Contributors. All rights reserved.
+# Copyright (c) 2021-2025, Arm Limited and Contributors. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
@@ -12,6 +12,8 @@ set(CMAKE_SYSTEM_PROCESSOR "aarch64")
 set(SCP_AARCH64_PROCESSOR_TARGET "cortex-a57.cortex-a53")
 
 set(CMAKE_TOOLCHAIN_PREFIX "aarch64-none-elf-")
+
+string(APPEND CMAKE_EXE_LINKER_FLAGS_INIT "-nostdlib ")
 
 include(
     "${CMAKE_CURRENT_LIST_DIR}/../../../cmake/Toolchain/GNU-Baremetal.cmake")
--- product/rcar/scp_ramfw/rcar_libc.c
+++ product/rcar/scp_ramfw/rcar_libc.c
@@ -14,6 +14,10 @@
 #include <stdio.h>
 #include <string.h>
 
+extern void panic(void);
+
+int __errno;
+
 void *memset(void *s, int c, size_t count)
 {
     char *xs = s;
@@ -268,4 +272,61 @@ void __assert_fail(
 {
     while (1)
         continue;
+}
+
+extern char __HEAP_START__;
+extern char __HEAP_END__;
+
+/*!
+ * \brief Architecture memory manager context.
+ */
+static struct arch_mm_ctx {
+    uintptr_t heap_break;
+    uintptr_t heap_end;
+} arch_mm_ctx = {
+    .heap_break = (uintptr_t)(&__HEAP_START__),
+    .heap_end = (uintptr_t)(&__HEAP_END__),
+};
+
+void *sbrk(intptr_t increment)
+{
+    if (increment == 0) {
+        return (void *)arch_mm_ctx.heap_break;
+    } else {
+        uintptr_t heap_old = FWK_ALIGN_NEXT(arch_mm_ctx.heap_break, 16);
+        uintptr_t heap_new = heap_old + increment;
+
+        if (heap_new > arch_mm_ctx.heap_end) {
+            return (void *)-1;
+        } else {
+            arch_mm_ctx.heap_break = heap_new;
+
+            return (void *)heap_old;
+        }
+    }
+}
+
+void *malloc(size_t size)
+{
+    void *mem = sbrk((intptr_t)size);
+
+    if (mem == ((void *)-1))
+        mem = NULL;
+
+    return mem;
+}
+
+void *calloc(size_t nmemb, size_t size)
+{
+    void *mem = malloc(nmemb * size);
+
+    if (mem)
+        memset(mem, 0, nmemb * size);
+
+    return mem;
+}
+
+void free(void *ptr)
+{
+    panic();
 }
